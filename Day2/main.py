fc5aca5621bc277e29c6019fd7e24f3250b79458
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-03-03 06:35:25 +0200

    added all

---
+++ Day2/main.py
@@ -0,0 +1,27 @@
+
+#Q1
+# str = input("Enter a string: ")
+# ls=[]
+# for index,char in enumerate(str):
+#     if(char == 'i'):
+#         ls.append(index)
+# print(ls)
+
+
+#Q2
+# num = input("Enter a number: ")
+# ls = []
+# for i in range(1 , int(num)+1):
+#     ls2 = []
+#     for j in range(1, int(i) + 1):
+#         ls2.append(j*i)
+#     ls.append(ls2)
+# print(ls)
+
+
+#Q3
+# names = ['Hamdy','Mostafa','Omer','Hoba']
+# dec = {}
+# for name in names:
+#     dec[name[0]] = name
+# print(dec)
